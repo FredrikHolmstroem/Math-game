2c9c987f4a9fe7993f5a3ea11345c5da10ae4807
FredrikHolmstroem <129791809+FredrikHolmstroem@users.noreply.github.com>
2023-04-05 13:11:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const nextBtn = document.getElementById("next-btn");
 
 document.getElementById("start-random").addEventListener("click", () => startGame("random"));
 document.getElementById("start-chain-rule").addEventListener("click", () => startGame("chainRule"));
-document.getElementById("start-chain-rule-2").addEventListener("click", () => startGame("chainRule2"));
 
 function startGame(gameType) {
     gameMenu.style.display = "none";
@@ -17,14 +16,7 @@ function startGame(gameType) {
 
 function generateNewProblem() {
     solutionDiv.style.display = "none";
-    let problem;
-    if (gameMode === "random") {
-        problem = generateRandomProblem();
-    } else if (gameMode === "chainRule") {
-        problem = generateChainRuleProblem(false);
-    } else if (gameMode === "chainRule2") {
-        problem = generateChainRuleProblem(true);
-    }
+    const problem = gameMode === "random" ? generateRandomProblem() : generateChainRuleProblem();
     currentAnswer = calculateAnswer(problem);
     const options = generateOptions(currentAnswer);
     displayProblem(problem);
@@ -40,7 +32,7 @@ function generateRandomProblem() {
     return { a, b, operator };
 }
 
-function generateChainRuleProblem(isLevel2) {
+function generateChainRuleProblem() {
     const functions = [
         { fn: 'x^2', derivative: '2x' },
         { fn: 'sin(x)', derivative: 'cos(x)' },
@@ -48,11 +40,6 @@ function generateChainRuleProblem(isLevel2) {
         { fn: 'e^x', derivative: 'e^x' },
         { fn: 'ln(x)', derivative: '1/x' },
     ];
-    if (isLevel2) {
-        functions.push({ fn: 'sin(x+1)', derivative: 'cos(x+1)' });
-        functions.push({ fn: 'cos(x+1)', derivative: '-sin(x+1)' });
-        functions.push({ fn: 'x^2+1', derivative: '2x' });
-    }
     const a = functions[Math.floor(Math.random() * functions.length)];
     const b = functions[Math.floor(Math.random() * functions.length)];
 
@@ -81,56 +68,51 @@ function generateOptions(answer) {
             { fn: 'x^2', derivative: '2x' },
             { fn: 'sin(x)', derivative: 'cos(x)' },
             { fn: 'cos(x)', derivative: '-sin(x)' },
-{ fn: 'e^x', derivative: 'e^x' },
-{ fn: 'ln(x)', derivative: '1/x' },
-];
-if (gameMode === "chainRule2") {
-functions.push({ fn: 'sin(x+1)', derivative: 'cos(x+1)' });
-functions.push({ fn: 'cos(x+1)', derivative: '-sin(x+1)' });
-functions.push({ fn: 'x^2+1', derivative: '2x' });
-}
-while (options.size < 4) {
-const a = functions[Math.floor(Math.random() * functions.length)];
-const b = functions[Math.floor(Math.random() * functions.length)];
-options.add((${a.derivative})(${b.fn}) + (${a.fn})(${b.derivative}));
-}
-}
-return Array.from(options);
+            { fn: 'e^x', derivative: 'e^x' },
+            { fn: 'ln(x)', derivative: '1/x' },
+        ];
+        while (options.size < 4) {
+            const a = functions[Math.floor(Math.random() * functions.length)];
+            const b = functions[Math.floor(Math.random() * functions.length)];
+            options.add(`(${a.derivative})(${b.fn}) + (${a.fn})(${b.derivative})`);
+        }
+    }
+    return Array.from(options);
 }
 
 function displayProblem(problem) {
-const problemElement = document.getElementById("problem");
-if (problem.operator === "'") {
-problemElement.innerHTML = y = \\(${problem.a.fn}\\)\\(${problem.b.fn}\\);
-} else {
-problemElement.innerHTML = \\(${problem.a} ${problem.operator} ${problem.b}\\);
-}
-MathJax.typeset(); // Update the LaTeX rendering
+    const problemElement = document.getElementById("problem");
+        if (problem.operator === "'") {
+        problemElement.innerHTML = `y = \\(${problem.a.fn}\\)\\(${problem.b.fn}\\)`;
+    } else {
+        problemElement.innerHTML = `\\(${problem.a} ${problem.operator} ${problem.b}\\)`;
+    }
+    MathJax.typeset(); // Update the LaTeX rendering
 }
 
 function displayOptions(options) {
-options.forEach((option, index) => {
-const optionElement = document.getElementById(option${index + 1});
-if (gameMode === "chainRule" || gameMode === "chainRule2") {
-optionElement.innerHTML = \\(${option}\\);
-} else {
-optionElement.textContent = option;
-}
-optionElement.onclick = () => checkAnswer(option);
-});
-MathJax.typeset(); // Update the LaTeX rendering
+    options.forEach((option, index) => {
+        const optionElement = document.getElementById(`option${index + 1}`);
+        if (gameMode === "chainRule") {
+            optionElement.innerHTML = `\\(${option}\\)`;
+        } else {
+            optionElement.textContent = option;
+        }
+        optionElement.onclick = () => checkAnswer(option);
+    });
+    MathJax.typeset(); // Update the LaTeX rendering
 }
 
 function checkAnswer(selectedOption) {
-if (selectedOption === currentAnswer) {
-if (gameMode === "chainRule" || gameMode === "chainRule2") {
-correctAnswerElem.innerHTML = Correct answer: \\(${currentAnswer}\\)<br>Step-by-step solution (simplified):<br>dy/dx = \\(${currentAnswer}\\);
-} else {
-correctAnswerElem.textContent = Correct answer: ${currentAnswer};
-}
-solutionDiv.style.display = "block";
-MathJax.typeset(); // Update the LaTeX rendering
-}
+    if (selectedOption === currentAnswer) {
+        if (gameMode === "chainRule") {
+            correctAnswerElem.innerHTML = `Correct answer: \\(${currentAnswer}\\)<br>Step-by-step solution (simplified):<br>dy/dx = \\(${currentAnswer}\\)`;
+        } else {
+            correctAnswerElem.textContent = `Correct answer: ${currentAnswer}`;
+        }
+        solutionDiv.style.display = "block";
+        MathJax.typeset(); // Update the LaTeX rendering
+    }
 }
 
 let gameMode;
