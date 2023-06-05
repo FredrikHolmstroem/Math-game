18a69a0682736b2e55a3e0aba505ea02d528bc6f
FredrikHolmstroem <129791809+FredrikHolmstroem@users.noreply.github.com>
2023-06-05 21:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-let difficulty;
-let correctAnswer;
+const operators = ['^'];
+const variables = ['a', 'b', 'c', 'x', 'y', 'z'];
 
-function startGame(level) {
-    difficulty = level;
-    document.getElementById('difficulty').style.display = 'none';
-    document.getElementById('game').style.display = 'block';
-    generateProblem();
+function TreeNode(left, right, operator) {
+  this.left = left;
+  this.right = right;
+  this.operator = operator;
+
+  this.toString = function() {
+    return '(' + left + ' ' + operator + ' ' + right + ')';
+  }
 }
 
-function generateProblem() {
-    let base = Math.ceil(Math.random() * 10);
-    let exponent = Math.ceil(Math.random() * difficulty);
-    document.getElementById('problem').innerText = `${base} ^ ${exponent}`;
-    correctAnswer = Math.pow(base, exponent);
+function randomNumberRange(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function checkAnswer() {
-    let userAnswer = document.getElementById('answer').value;
-    if (userAnswer == correctAnswer) {
-        document.getElementById('feedback').innerText = 'Rätt svar! Det korrekta svaret var: ' + correctAnswer;
-    } else {
-        document.getElementById('feedback').innerText = 'Fel svar. Det korrekta svaret var: ' + correctAnswer;
-    }
-    generateProblem();
+function randomVariable() {
+  return variables[randomNumberRange(0, variables.length)];
 }
+
+function buildTree(numNodes) {
+  if (numNodes === 1)
+    return randomVariable();
+
+  const numLeft = Math.floor(numNodes / 2);
+  const leftSubTree = buildTree(numLeft);
+  const numRight = Math.ceil(numNodes / 2);
+  const rightSubTree = buildTree(numRight);
+  const operator = operators[randomNumberRange(0, operators.length)];
+
+  return new TreeNode(leftSubTree, rightSubTree, operator);
+}
+
+document.getElementById('easy').addEventListener('click', function() {
+  const expression = buildTree(2);
+  document.getElementById('question').innerText = expression.toString();
+});
+
+document.getElementById('medium').addEventListener('click', function() {
+  const expression = buildTree(3);
+  document.getElementById('question').innerText = expression.toString();
+});
+
+document.getElementById('hard').addEventListener('click', function() {
+  const expression = buildTree(4);
+  document.getElementById('question').innerText = expression.toString();
+});
+
+document.getElementById('submit').addEventListener('click', function() {
+  const answer = document.getElementById('answer').value;
+  // Rätt svar beräkning logik här
+});
